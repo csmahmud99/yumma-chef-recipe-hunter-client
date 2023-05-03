a22a09c9156e3e4dfd36f8a7bc808527b68a43fa
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-03 18:41:44 +0600

    Footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <div style={{backgroundColor: "#4A4A4A"}}>
+            <Container>
+                <div className="text-center text-white p-5">
+                    <p>
+                        Copyright &copy; 2023 | <strong>Yumma Chef Recipe Hunter</strong> by <a href="" target="_blank" style={{color: "#FFC107"}} className="fw-semibold text-decoration-none">CS Mahmud</a> | All Rights Reserved.
+                    </p>
+                </div>
+            </Container>
         </div>
     );
 };
