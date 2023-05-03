c8bd20d5e8af0ad7977faba3fdc6c86f367120dc
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-03 20:12:39 +0600

    Home Page Banner Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import banner from "../../assets/yumma-home-banner-left.png";
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <div className="bg-dark text-white py-5">
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={banner} alt="home-banner" className="rounded border border-warning border-5 img-fluid" />
+                        </Col>
+
+                        <Col className="mt-3">
+                            <p className="text-center text-info">Who We Are</p>
+                            <h3 className="text-warning text-center">Yumma Chef Recipe</h3>
+                            <p className="text-center">
+                                Get all delicious food recipes from your favorite chef.
+                            </p>
+                            <hr />
+                            <p className="text-center">
+                                Welcome to Yumma! We offer easy-to-follow recipes for diverse dishes, catering to all tastes. Inspiring beginners and experienced chefs alike, our mission is to create delicious meals. Explore our collection and let's cook up something amazing together!
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
         <div style={{backgroundColor: "#4A4A4A"}}>
             <Container>
                 <div className="text-center text-white p-5">
-                    <p>
-                        Copyright &copy; 2023 | <strong>Yumma Chef Recipe Hunter</strong> by <a href="" target="_blank" style={{color: "#FFC107"}} className="fw-semibold text-decoration-none">CS Mahmud</a> | All Rights Reserved.
+                    <p className="bg-dark p-5 rounded">
+                        Copyright &copy; 2023 | <strong>Yumma Chef Recipe Hunter</strong> by <a href="https://github.com/csmahmud99" target="_blank" style={{color: "#FFC107"}} className="fw-semibold text-decoration-none">CS Mahmud</a> | All Rights Reserved.
                     </p>
                 </div>
             </Container>
