2ff22deb817728f3f20d4b5acac3cf26b0498f6d
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-04 13:04:33 +0600

    Connected with Firbase & Context API Created

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './routes/Routes.jsx';
 import { RouterProvider } from 'react-router-dom';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+    <AuthProvider>
+        <RouterProvider router={router} />
+    </AuthProvider>
+    
 );
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Button, Container, Nav, Navbar, Stack } from 'react-bootstrap';
 import logo from "../../../assets/yumma_logo.png"
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <div>
             <Navbar className="pb-4" collapseOnSelect expand="lg" bg="warning" variant="light">
@@ -13,16 +17,20 @@ const Header = () => {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto  mt-3">
+                        <Nav className="me-auto mt-3">
                             <Link to="/" className="me-3"><Button variant="link" className="text-decoration-none text-dark fw-semibold">Home</Button></Link>
 
                             <Link to="/blog"><Button variant="link" className="text-decoration-none text-dark fw-semibold">Blog</Button></Link>
                         </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
+                        <Nav className="mt-3">
+                            <Stack direction="horizontal" gap={3}>
+                                {user && <FaUserCircle className="fs-2" />}
+                                {
+                                    user ?
+                                        <Button variant="dark">Logout</Button>
+                                        : <Link to="/login"><Button variant="dark">Login</Button></Link>
+                                }
+                            </Stack>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
